42b8f972bae56659a7f74b5951f8cc11c122e640
tomohiro ueno <hagurono9@gmail.com>
2024-01-26 12:21:49 +0900

    add

--- lib/screen/category_screen.dart
+++ lib/screen/category_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_stripe_application/models/category.dart';
 
 class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key});
@@ -10,6 +12,20 @@ class CategoriesScreen extends StatefulWidget {
 class _CategoriesScreenState extends State<CategoriesScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Categories'),
+      ),
+      body: MasonryGridView.count(
+        crossAxisCount: 3,
+        itemCount: Category.categories.length,
+        itemBuilder: (context, index) {
+          return Container(
+            color: Colors.primaries[index % Colors.primaries.length],
+            height: 100,
+          );
+        },
+      ),
+    );
   }
 }
